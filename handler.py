86e29e00a7e04306abecbc1e85a693e259766a44
Arvind S Panicker <arvindspanicker@gmail.com>
2018-07-28 03:58:26 +0530

    Changed to class and added conf file

--- handler.py
+++ handler.py
@@ -1,85 +1,180 @@
-import ari
-import logging
-import urllib2, json
-#import sqllite3
+import ari,time,urllib2,json,datetime
+# import sqlite3
+# import MySQLdb
 from hubspot.connection import APIKey, PortalConnection
 from hubspot.contacts import Contact
 from hubspot.contacts import save_contacts
-import datetime
+from ConfigParser import ConfigParser
+import logging,time
+from logging.handlers import RotatingFileHandler
 
-'''
-All errors are logged in a file called Handler.log
-'''
-logging.basicConfig(filename='Handler.log',level=logging.ERROR)
-client = ari.connect('http://192.168.20.195:8088/', 'arvind', 'arvind')
 
-def fetch_from_db(user_name, phone_number):
-    try:
+
+
+class AsteriskListener:
+
+    def __init__(self,log_file_name,conf_file_name):
+        self.log_file_name = log_file_name
+        self.conf_file_name = conf_file_name
+        self.config = ConfigParser()
+        self.path = conf_file_name
+        self.config.read(self.path)
+
+    def init_lisener(self):
         '''
-        fetch from the database for the email and all the other details based on the incoming name
-        and number 
+        function to initialize all the ARI configuration and create a listening event
         '''
-        # connection = MySQLdb.connect("localhost","testuser","test123","Contacts" )
-        connection = sqllite3.connect('Contact.db')
-        sql_command = "SELECT id,fname,lname,email from Contacts where uname = {user_name} and pnumber = \
-        {phone_number}".format(user_name=user_name,phone_number=phone_number)
-        crsr = connection.cursor()
-        crsr.execute(sql_command)
-        ans= crsr.fetchone() 
-        if len(ans):
-            return ans[0],ans[1],ans[2],ans[3] 
-        else:
-            logging.error('User Does Not Exist in Database')
-    except Exception as e:
-        logging.error(datetime.datetime.now()+e.message)
+        self.obtain_ari_config()
+        self.client.on_channel_event('StasisStart', self.stasis_start_cb)
+        self.client.on_channel_event('StasisEnd', self.stasis_end_cb)
+        'create this app in the extensions for statis event to occur'
+        self.client.run(apps='hello-world')
 
-def save_contact_to_hub(vid,first_name,last_name,email,phone_number):
-    ''' calls hubspot api and created a contact (if not exists) and saves it to 
-        the HubSpot Account registered 
-    '''
-    try:
-        authentication_key = APIKey("3dd24ad4-62c4-4d39-bacf-7616362648b2")
-        with PortalConnection(authentication_key, "Test_App") as connection:
-            contact = []
-            contact.append(Contact(vid=vid, email_address=unicode(email,'utf-8'), \
-            properties={u'lastname':unicode(last_name,'utf-8'), u'firstname': unicode(first_name,'utf-8'),\
-            u'phone':unicode(phone_number,'utf-8')},))
-            save_contacts(contact,connection)
-    except Exception as e:
-        logging.error(datetime.datetime.now()+e.message)
- 
+    def init_logger(self):
+        '''
+        function to initialize logging
+        '''
+        self.file_size = int(self.config.get('logfile-config', 'SIZE'))
+        self.back_up_count = int(self.config.get('logfile-config', 'BACKUPCOUNT'))
+        self.logger_name = self.config.get('logfile-config', 'LOGGERNAME')
+        self.logger = logging.getLogger(self.logger_name)
+        self.logger.setLevel(logging.DEBUG)
+        self.handler = RotatingFileHandler(self.log_file_name,maxBytes=self.file_size, backupCount=self.back_up_count)
+        self.formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s","%Y-%m-%d %H:%M:%S")
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
 
-def stasis_end_cb(channel, ev):
-    """Handler for StasisEnd event"""
-    """When caller cuts the call """
-    print "Channel %s just left our application" % channel.json.get('name')
-
-
-def stasis_start_cb(channel_obj, ev):
-    """Handler for StasisStart event"""
-    """When caller starts the call """
-    try:
-        ch = channel_obj.get('channel')
-        channel = ev.get('channel')
-        caller_name = channel.get('caller').get('name')
-        caller_number = channel.get('caller').get('number')
-        channel_id = channel.get('id')
-        print caller_name,caller_number,channel_id
-	'''
-	uncomment the following two lines to get connection to the established database
-	'''
-        # vid, first_name, last_name, email = fetch_from_db(caller_name, caller_number)
-        # save_contact_to_hub(vid,first_name,last_name,email,str(caller_number)) 
-        """
-        continue back to the dial plan
-        """
-        ch.continueInDialplan() 
-    except Exception as e:
-        logging.error(datetime.datetime.now()+e.message)
+    def obtain_ari_config(self):
+        '''
+        function to get the configuration of ARI from the config file
+        '''
+        try:
+            self.logger.debug('Inside obtain_ari_config() function')
+            
+            self.ari_host = self.config.get('ari-config', 'ARI_IP')
+            self.ari_port = self.config.get('ari-config', 'ARI_PORT')
+            self.ari_username = self.config.get('ari-config','ARI_USERNAME')
+            self.ari_password = self.config.get('ari-config','ARI_PASSWORD')
+            self.client = ari.connect('http://{host}:{port}/'.format(host=self.ari_host,port=self.ari_port),self.ari_username,self.ari_password)
+        except Exception as e:
+            self.logger.exception(e.message)
 
 
-client.on_channel_event('StasisStart', stasis_start_cb)
-client.on_channel_event('StasisEnd', stasis_end_cb)
+    def obtain_db_config(self):
+        '''
+        function to obtain all the database configurations from the configuration file
+        '''
+        try:
+            self.logger.debug('Inside obtain_db_config() function')
+            self.db_host = self.config.get('db-config', 'DB_IP', '127.0.0.1')
+            self.db_username = self.config.get('db-config','DB_USERNAME')
+            self.db_password = self.config.get('db-config','DB_PASSWORD')
+            self.db_name = self.config.get('db-config','DB_NAME')
+            self.fname = self.config.get('db-config','DB_FIELD_FNAME')
+            self.lname = self.config.get('db-config','DB_FIELD_LNAME')
+            self.email = self.config.get('db-config','DB_FIELD_EMAIL')
+        except Exception as e:
+            self.logger.exception(e.message)
 
-'create this app in the extensions for statis event to occur'
-client.run(apps='hello-world')
+
+    def obtain_hubspot_config(self):
+        '''
+        function to obtain all the configurations for the Hubspot API from the configuration file
+        '''
+        try:
+            self.logger.debug('Inside obtain_hubspot_config() function')
+            self.api_key = self.config.get('hubspot-config', 'HUBSPOT_API_KEY')
+            self.app_name = self.config.get('hubspot-config','APP_NAME')
+        except Exception as e:
+            self.logger.exception(e.message)      
+
+    
+    def fetch_from_db(self):
+        '''
+            fetch from the database for the email and all the other details based on the incoming name
+            and number 
+        '''
+        try:
+            self.logger.debug('Inside fetch_from_db() function')
+            self.obtain_db_config()
+            connection = MySQLdb.connect(self.db_host,self.db_username,self.db_password,self.db_name)
+            '''
+            uncomment below if you're using sqlite3 database and comment the above line
+            '''
+            # connection = sqllite3.connect(self.db_name)
+            sql_command = "SELECT id,{fname},{lname},{email} from Contacts where pnumber = \
+            {phone_number}".format(phone_number=phone_number,fname=fname,lname=lname,email=email)
+            crsr = connection.cursor()
+            crsr.execute(sql_command)
+            ans= crsr.fetchone() 
+            if len(ans):
+                self.vid = ans[0]
+                self.first_name = ans[1]
+                self.last_name = ans[2]
+                self.email = ans[3] 
+            else:
+                self.logger.error('User Does Not Exist in Database') 
+        except Exception as e:
+            self.logger.error(e.message)
+
+    def save_contact_to_hub(self):
+        ''' calls hubspot api and created a contact (if not exists) and saves it to 
+            the HubSpot Account registered 
+        '''
+        try:
+            self.logger.debug('Inside save_contact_to_hub() function')
+            self.logger.debug("Saving contact to hub {}{}{}{}{}".format(self.vid,self.first_name,self.last_name,self.email,self.phone_number))
+            self.obtain_hubspot_config()
+            authentication_key = APIKey(self.api_key)
+            with PortalConnection(authentication_key, self.app_name) as connection:
+                contact = []
+                contact.append(Contact(vid=self.vid, email_address=unicode(self.email,'utf-8'), \
+                properties={u'lastname':unicode(self.last_name,'utf-8'), u'firstname': unicode(self.first_name,'utf-8'),\
+                u'phone':unicode(str(self.phone_number),'utf-8')},))
+                save_contacts(contact,connection)
+        except Exception as e:
+            self.logger.error(e.message)
+    
+
+    def stasis_end_cb(self,channel, ev):
+        """Handler for StasisEnd event"""
+        """When caller cuts the call """
+        try:
+            self.logger.debug('Inside stasis_end_cb() function')
+            print "Channel %s just left our application" % self.channel.get('name')
+        except Exception as e:
+            self.logger.error(e.message)
+
+
+    def stasis_start_cb(self,channel_obj, ev):
+        """Handler for StasisStart event"""
+        """When caller starts the call """
+        try:
+            self.logger.debug('Inside stasis_start_cb() function')
+            self.ch = channel_obj.get('channel')
+            self.channel = ev.get('channel')
+            self.caller_name = self.channel.get('caller').get('name')
+            self.caller_number = self.channel.get('caller').get('number')
+            self.channel_id = self.channel.get('id')
+            self.logger.debug('Call from Phone Number {}'.format(self.caller_number))
+            '''
+            uncomment the following two lines to get connection to the established database
+            '''
+            # self.fetch_from_db()
+            # self.save_contact_to_hub() 
+            """
+            continue back to the dial plan
+            """
+            self.ch.continueInDialplan() 
+        except Exception as e:
+            self.logger.error(e.message)
+
+
+
+if __name__ == "__main__":
+    log_file_name = 'Handler.log'
+    conf_file_name = 'handler.conf'
+    asterisk_lisener = AsteriskListener(log_file_name,conf_file_name)
+    asterisk_lisener.init_logger()
+    asterisk_lisener.init_lisener()
+
