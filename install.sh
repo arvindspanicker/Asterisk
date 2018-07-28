4e0c61c961c0b2148c60bd2d52f77516b4eb3dbb
Arvind S Panicker <arvindspanicker@gmail.com>
2018-07-28 05:30:23 +0530

    Requirements.txt

--- install.sh
+++ install.sh
@@ -3,7 +3,5 @@ sudo apt-get install -y python-pip python-dev
 pip install virtualenv
 virtualenv asterisk --python=python2.7
 source asterisk/bin/activate
-pip install swaggerpy
-pip install git+https://github.com/asterisk/ari-py.git
-pip install hubspot-contacts
+pip install -r requirements.txt
 
