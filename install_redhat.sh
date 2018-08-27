00dce50c0b982f753e5c6afc07c48135e4421266
Arvind S Panicker <arvindspanicker@gmail.com>
2018-08-27 10:45:50 +0530

    install script for redhat

---
+++ install_redhat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+APP_DIRECTORY=$(pwd)
+yum install gcc
+cd /usr/src
+wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+tar xzf Python-2.7.10.tgz
+cd Python-2.7.10
+./configure
+make altinstall
+yum --enablerepo=extras install -y  epel-release
+sudo yum install -y python-pip
+sudo yum install -y git
+pip install virtualenv
+cd $APP_DIRECTORY
+virtualenv asterisk --python=python2.7
+source asterisk/bin/activate
+pip install -r requirements.txt
+
+
+
